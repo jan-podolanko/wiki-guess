29a391334015ca02d7ef2c1f52352e91d06d335e
jan-podolanko <jan.podolanko11@gmail.com>
2023-05-16 18:23:18 +0200

    init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onBeforeMount, ref } from 'vue';
+import TheWelcome from '../components/TheWelcome.vue'
+
+let query: any;
+
+
+async function getData() {
+  const response = await fetch("https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=5&format=json&origin=*");
+  const data = await response.json();
+  console.log(data)
+  return data;
+}
+
+onBeforeMount(()=> {
+  getData().then(data => {
+    query = data;
+  })
+})
+
+
+</script>
+
+<template>
+  <main>
+    <div>{{ query.query }}</div>
+  </main>
+</template>
