557e5f7290bd5a24c39be3cfc028fbdf481816b1
jan-podolanko <jan.podolanko11@gmail.com>
2023-05-16 23:21:45 +0200

    get wikipedia article with action api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue';
-import TheWelcome from '../components/TheWelcome.vue'
+import { onBeforeMount, reactive, ref } from 'vue';
 
-let query: any;
+const data = reactive({
+  query:[],
+  article:[],
+})
+// to get content
+// prop=revisions&rvprop=content
 
 
 async function getData() {
-  const response = await fetch("https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=5&format=json&origin=*");
+/*   const response = await fetch("https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&prop=revisions&rvprop=content&format=json&origin=*"); */
+  const response = await fetch("https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=json&origin=*");  
   const data = await response.json();
-  console.log(data)
+  console.log(data);
   return data;
 }
 
+async function getContent(title: string) {
+/*   const response = await fetch("https://en.wikipedia.org/w/api.php?action=query&titles=" + title + "&prop=revisions|pageviews&rvprop=content&rvslots=*&format=json&origin=*");  
+ */  
+  const response = await fetch("https://en.wikipedia.org/w/api.php?action=query&titles=" + title + "&prop=revisions|pageviews&rvprop=content&rvslots=*&format=json&origin=*");  
+  const article = await response.json();
+  console.log(article);
+  return article;
+}
+
 onBeforeMount(()=> {
-  getData().then(data => {
-    query = data;
+  getData().then(response => {
+    const title = response.query.random[0].title;
+    const id = response.query.random[0].id;
+    data.query = response;
+    const content = getContent(title).then(content => {
+      data.article = content.query.pages[id].revisions;
+    });
   })
 })
 
@@ -23,6 +42,7 @@ onBeforeMount(()=> {
 
 <template>
   <main>
-    <div>{{ query.query }}</div>
+    <div>{{ data.query.query.random[0].title }}</div>
+    <div>{{ data.article[0].slots.main["*"] }}</div>
   </main>
 </template>
